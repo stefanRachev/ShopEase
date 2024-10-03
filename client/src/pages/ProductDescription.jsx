cde9db8c72d02cbefdf5be65f6b5b9dccb7c348e
stefanRachev <starfystef@gmail.com>
2024-10-03 20:49:56 +0300

    change product component

--- client/src/pages/ProductDescription.jsx
+++ client/src/pages/ProductDescription.jsx
@@ -1,5 +1,4 @@
-// ProductDescription.jsx
-import styles from './ProductDescription.module.css';
+import styles from "./ProductDescription.module.css";
 import { useLanguage } from "../context/useLanguage";
 
 function ProductDescription() {
@@ -10,8 +9,7 @@ function ProductDescription() {
       id: 1,
       name: translations[language].product1,
       description: translations[language].description1,
-      price: "100.00",
-      image: "https://via.placeholder.com/300x300",
+      image: "https://i.imgur.com/RxODxfH.jpg",
     },
     {
       id: 2,
@@ -27,37 +25,70 @@ function ProductDescription() {
       price: "200.00",
       image: "https://via.placeholder.com/300x300",
     },
+    {
+      id: 4,
+      name: translations[language].product4,
+      description: translations[language].description4,
+      price: "250.00",
+      image: "https://via.placeholder.com/300x300",
+    },
   ];
 
   return (
-    <div className={styles.productContainer}>
-      <h2>{translations[language].featuredProducts}</h2>
-      <div className="row">
-        {products.map((product) => (
-          <div key={product.id} className="col-md-4">
-            <div className={`card ${styles.productCard}`}>
-              <img
-                src={product.image}
-                className={`card-img-top ${styles.productImage}`}
-                alt={product.name}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">{product.description}</p>
-                <p className="card-text">
-                  <strong>{product.price} лв.</strong>
-                </p>
-                <button className="btn btn-primary">
-                  {translations[language].addToCart}
-                </button>
-              </div>
+    <div
+      id="carouselExample"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        {products.map((product, index) => (
+          <div
+            key={product.id}
+            className={`carousel-item ${index === 0 ? "active" : ""}`}
+          >
+            <div className="d-flex justify-content-around">
+              {products.map((prod) => (
+                <div key={prod.id} className={`card ${styles.cardCustom}`}>
+                  <img
+                    src={prod.image}
+                    className={`card-img-top ${styles.cardImage}`}
+                    alt={prod.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{prod.name}</h5>
+                    <p className="card-text">{prod.description}</p>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         ))}
       </div>
+
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">
+          {language === "en" ? "Previous" : "Предишен"}
+        </span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">
+          {language === "en" ? "Next" : "Следващ"}
+        </span>
+      </button>
     </div>
   );
 }
 
 export default ProductDescription;
-
