8d87b1452d932adc28d04a858ea0883907a74e66
stefanRachev <starfystef@gmail.com>
2024-10-09 18:07:35 +0300

    clear comments in component

--- client/src/pages/NutritionalSupplements.jsx
+++ client/src/pages/NutritionalSupplements.jsx
@@ -1,7 +1,7 @@
 
 import { useLanguage } from "../context/useLanguage";
 import { Link } from "react-router-dom";
-import Slider from "react-slick"; // Импортираме Slider от react-slick
+import Slider from "react-slick"; 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -36,31 +36,31 @@ function NutritionalSupplements() {
       image: "https://i.imgur.com/HpibGkT.jpg",
       link: "/weight-plates",
     },
-    // Добави още продукти, ако желаеш
+    
   ];
 
-  // Настройки за слайдера
+  
   const sliderSettings = {
-    dots: false, // Не показва точки
+    dots: false, 
     infinite: true,
-    speed: 2000, // Скорост на плъзгане
-    slidesToShow: 3, // Показва 3 продукта едновременно
+    speed: 2000, 
+    slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true, // Автоматично плъзгане
-    autoplaySpeed: 3000, // Интервал на плъзгане
-    cssEase: "linear", // Плавно преливане
+    autoplay: true, 
+    autoplaySpeed: 3000, 
+    cssEase: "linear", 
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2, // Показва 2 продукта на таблети
+          slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1, // Показва 1 продукт на телефони
+          slidesToShow: 1, 
           slidesToScroll: 1,
         },
       },
