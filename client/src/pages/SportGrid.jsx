d1c0fb1f3d4efe827aaa37c67f1965a8b9023d50
stefanRachev <starfystef@gmail.com>
2024-10-13 12:47:06 +0300

    Refactor SportGrid page: Remove commented out code and add new sports

--- client/src/pages/SportGrid.jsx
+++ client/src/pages/SportGrid.jsx
@@ -1,48 +1,4 @@
 // SportGrid.jsx
-// import { Card, Row, Col } from "react-bootstrap";
-// import styles from "./SportGrid.module.css";
-
-// function SportGrid() {
-//   const sports = [
-//     {
-//       name: "Boxing",
-//       image: "https://i.imgur.com/XpjouuA.jpg",
-//       link: "/sports/boxing",
-//     },
-//     {
-//       name: "Yoga",
-//       image: "https://i.imgur.com/7xdqVAc.jpg",
-//       link: "/sports/yoga",
-//     },
-//     {
-//       name: "CrossFit",
-//       image: "https://i.imgur.com/LCP7znY.jpg",
-//       link: "/sports/crossFit",
-//     },
-//     // Добави още спортове тук
-//   ];
-
-//   return (
-//     <Row className="g-4">
-//       {sports.map((sport, index) => (
-//         <Col key={index} md={4} sm={6}>
-//           <a href={sport.link} className={styles.sportCard}>
-//             <Card className={styles.hoverCard}>
-//               <div className={styles.imageWrapper}>
-//                 <Card.Img variant="top" src={sport.image} alt={sport.name} />
-//               </div>
-//               <Card.Body>
-//                 <Card.Title className={styles.cardTitle}>{sport.name}</Card.Title>
-//               </Card.Body>
-//             </Card>
-//           </a>
-//         </Col>
-//       ))}
-//     </Row>
-//   );
-// }
-
-// export default SportGrid;
 
 import { useState } from "react";
 import { Card, Row, Col } from "react-bootstrap";
@@ -67,7 +23,22 @@ function SportGrid() {
       image: "https://i.imgur.com/LCP7znY.jpg",
       link: "/sports/crossFit",
     },
-    // Добави още спортове тук
+    {
+      name: "Swimming",
+      image: "https://i.imgur.com/OYoDbhQ.jpg",
+      link: "/sports/swimming",
+    },
+    {
+      name: "MMA",
+      image: "https://i.imgur.com/W6raofK.jpg",
+      link: "/sports/mma",
+    },
+    {
+      name: "Kickboxing",
+      image: "https://i.imgur.com/eEyP95l.jpg",
+      link: "/sports/kickboxing",
+    },
+    
   ];
 
   const handleTouchStart = (index) => {
@@ -86,7 +57,12 @@ function SportGrid() {
               onTouchStart={() => handleTouchStart(index)}
             >
               <div className={styles.imageWrapper}>
-                <Card.Img variant="top" src={sport.image} alt={sport.name} className="img-fluid"/>
+                <Card.Img
+                  variant="top"
+                  src={sport.image}
+                  alt={sport.name}
+                  className="img-fluid"
+                />
               </div>
               <Card.Body>
                 <Card.Title className={styles.cardTitle}>
