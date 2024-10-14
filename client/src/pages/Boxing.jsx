da728be41aeaae6f1da2579df8d414c6be4bf716
stefanRachev <starfystef@gmail.com>
2024-10-14 17:24:48 +0300

    Refactor Boxing page: Update product images and prices

--- client/src/pages/Boxing.jsx
+++ client/src/pages/Boxing.jsx
@@ -6,21 +6,21 @@ function Boxing() {
     {
       id: "boxing-1",
       name: "Боксови Ръкавици",
-      image: "https://i.imgur.com/abc123.jpg",
+      image: "https://i.imgur.com/5npwqHh.jpg",
       price: "45 лв.",
       description: "Комфортни и здрави боксови ръкавици за тренировки.",
     },
     {
       id: "boxing-2",
       name: "Боксов Чувал",
-      image: "https://i.imgur.com/def456.jpg",
+      image: "https://i.imgur.com/KU1nhkR.jpg",
       price: "120 лв.",
       description: "Тежък чувал за професионални тренировки.",
     },
     {
       id: "boxing-3",
       name: "Боксов Каска",
-      image: "https://i.imgur.com/ghi789.jpg",
+      image: "https://i.imgur.com/o8K4kBD.jpg",
       price: "75 лв.",
       description: "Лека каска за безопасност при спаринг.",
     },
@@ -39,17 +39,19 @@ function Boxing() {
       <Row className="g-4">
         {products.map((product) => (
           <Col key={product.id} md={4} sm={6} xs={12}>
-            <Card className="h-100">
+            <Card className="h-100 d-flex flex-column">
               <Card.Img
                 variant="top"
                 src={product.image}
                 alt={product.name}
                 className="img-fluid"
               />
-              <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text>{product.description}</Card.Text>
-                <div className="d-flex justify-content-between align-items-center">
+              <Card.Body className="d-flex flex-column justify-content-between">
+                <div>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>{product.description}</Card.Text>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mt-auto">
                   <span className="text-success fw-bold">{product.price}</span>
                   <Button variant="primary">Купи</Button>
                 </div>
