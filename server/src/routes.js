e1262f997a293c53a55b5eede889ed65492f5c34
stefanRachev <starfystef@gmail.com>
2024-10-18 17:48:03 +0300

    Add orderController and Order schema, and update routes.js

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userController = require("./controllers/userController");
+const orderController = require("./controllers/orderController");
 const authenticateToken = require("./middlewares/authMiddleware");
 
 router.get("/me", authenticateToken, userController.getMe);
@@ -7,6 +8,8 @@ router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout",authenticateToken, userController.logout);
 router.post("/refresh-token", userController.refreshToken);
+router.post("/orders", authenticateToken, orderController.createOrder);
+router.get("/orders", authenticateToken, orderController.getUserOrders);
 
 
 module.exports = router;
